a99893b0b841f120b03f4e99b8672cfebe3cd7fb
Ian Pickering <ipickering2@gmail.com>
2018-04-24 11:57:46 -0700

    Don't use obtainFileCopy as it can return a cached file with dummy ident

--- src/test/kotlin/com/ruin/lsp/EditorUtilTest.kt
+++ src/test/kotlin/com/ruin/lsp/EditorUtilTest.kt
@@ -1,6 +1,10 @@
 package com.ruin.lsp
 
+import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.application.runUndoTransparentWriteAction
+import com.intellij.psi.impl.PsiDocumentManagerBase
 import com.ruin.lsp.util.differenceFromAction
+import com.ruin.lsp.util.getDocument
 import org.eclipse.lsp4j.TextEdit
 import org.intellivim.FileEditingTestCase
 
@@ -26,6 +30,34 @@ class EditorUtilTest : FileEditingTestCase() {
             TextEdit(range(0, 0, 0, 0), "Hey, dood!"),
             TextEdit(range(2, 0, 2, 5), ""),
             TextEdit(range(2, 25, 4, 1), "blah")
+        ))
+    }
+
+    fun `test text edits after document change`() {
+        val doc = getDocument(psiFile)!!
+        runUndoTransparentWriteAction {
+            doc.insertString(0, "Cowabunga!")
+        }
+        val edits = differenceFromAction(psiFile) { editor, _ ->
+            editor.document.insertString(0, "Hey, dood!")
+        }
+        assertNotNull(edits)
+        assertSameElements(edits!!.toList(), listOf(
+            TextEdit(range(0, 0, 0, 0), "Hey, dood!")
+        ))
+    }
+
+    fun `test text edits after view provider document change`() {
+        val doc = psiFile.viewProvider.document!!
+        runUndoTransparentWriteAction {
+            doc.insertString(0, "Cowabunga!")
+        }
+        val edits = differenceFromAction(psiFile) { editor, _ ->
+            editor.document.insertString(0, "Hey, dood!")
+        }
+        assertNotNull(edits)
+        assertSameElements(edits!!.toList(), listOf(
+            TextEdit(range(0, 0, 0, 0), "Hey, dood!")
         ))
     }
 }
